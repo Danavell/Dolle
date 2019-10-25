3b9e6faaacc39faf89563f056d0c2e7fdbd90687
James <jamesdanavell@gmail.com>
2019-10-25 14:26:43 +0200

    Places csv file outside of main directory
    The csv files are too large to be stored on github and so have
    to be stored outside the git directory. A new function
    'get_csv_directory' in utils.utils will automatically return
    or create the correct directory

--- scripts/temp_py.py
+++ scripts/temp_py.py
@@ -3,10 +3,31 @@ import os
 from models import machines
 import utils.work_table.data_preparation as wt
 import utils.sensor_data.data_preparation as sd
+from utils.utils import get
+
+class PreProcess:
+    def __init__(self, base_data, feature_extractor, persister):
+        self._base_data = base_data
+        self._feature_extractor = feature_extractor
+        self._persister = persister
+
+    def get_base_data(self, stats=False):
+        self._base_data.get_base_data(stats=stats)
+
+    def feature_extraction(self):
+        self._feature_extractor.extract_features(
+            self._base_data.sensor_data
+        )
+
+    def save(self):
+        self._persister.save(**self._feature_extractor.data)
 
 
-machine = machines.Machine1405()
-columns = machine.data_generation_columns
+class SettingsThreeMainLadders1405:
+    def __init__(self, wt_csv, sd_csv):
+        machine = machines.Machine1405()
+        columns = machine.data_generation_columns
+        path =
 path = r'/home/james/Documents/Development/Dolle/csvs/'
 wt_path = os.path.join(path, 'WORK_TABLE.csv')
 sd_path = os.path.join(path, '01-01-18 to 01-01-19/datacollection.csv')
--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 
 
@@ -8,3 +10,29 @@ def get_dummy_products(data):
     return pd.concat([data, pd.get_dummies(data.loc[:, 'NAME'])], axis=1)
 
 
+def get_base_dolle_directory():
+    path = os.getcwd()
+    if path.split('/')[-1].lower() != 'dolle':
+        while True:
+            path = os.path.dirname(path)
+            last = path.split('/')[-1].lower()
+            if last == 'dolle':
+                break
+            if last == '/':
+                raise Exception('No Dolle Directory on the path')
+    return path
+
+
+def get_csv_directory():
+    path = os.getcwd()
+    if path.split('/')[-1].lower() != 'dolle':
+        while True:
+            path = os.path.dirname(path)
+            last = path.split('/')[-1].lower()
+            if last == 'dolle':
+                path = os.path.dirname(path)
+                path = os.path.join(path, 'dolle_csvs')
+                if not os.path.exists(path):
+                    os.mkdir(path)
+                break
+    return path
